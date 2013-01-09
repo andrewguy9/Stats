3db6fd6ccc14ae9c15e0ef681de29c4a74861eca
Andrew Thomson <andrewguy9@Andrews-MacBook-Pro.local>
2013-01-08 20:31:26 -0800

    Counter's no longer have get() method.
    Now you arrive at get by just invoking the functor
    with no args.

--- stats/counter.py
+++ stats/counter.py
@@ -8,12 +8,10 @@ class CumlativeMovingAverage:
     self.n = 0.0
     self.total = 0.0
 
-  def __call__(self, value):
-    self.n += 1
-    self.total += value
-    return self.get()
-
-  def get(self):
+  def __call__(self, value=None):
+    if value is not None:
+      self.n += 1
+      self.total += value
     return self.total / self.n
 
 # Exponentially moving average: Returns a moving average where a given data point has
@@ -24,16 +22,14 @@ class CumlativeMovingAverage:
 # S(t) = alpha * Y(t) + (1 - alpha) * S(t-1)
 # where alpha is between 0 and 1
 class ExponentialMovingAverage:
-  def __init__(self):
-    self.s = 0.0
+  def __init__(self, initial):
+    self.s = float(initial)
  
-  def __call__(self, y, alpha):
-    self.s = alpha * y + (1-alpha)*self.s
+  def __call__(self, y = None, alpha = None):
+    assert((y is None) == (alpha is None))
+    if y is not None:
+      self.s = alpha * y + (1-alpha)*self.s
     return self.s
-
-  def get(self):
-    return self.s
-
 
 # S(n) = a*Y(n) + (1-a)*S(n-1)
 # Where a is function of time between n and n-1.
@@ -41,37 +37,33 @@ class ExponentialMovingAverage:
 # Where time is in seconds and W is the period of time in minutes over which the
 # average is said to be over.
 class MovingAverage:
-  def __init__(self, period, time_source=time.time):
-    self.w = period
-    self.ts = time_source
-    self.t_last = self.ts()
-    self.avg = ExponentialMovingAverage()
+  def __init__(self, period, init_value, init_time):
+    self.w = float(period)
+    self.t_last = float(init_time)
+    self.avg = ExponentialMovingAverage(init_value)
 
-  def __call__(self, value):
-    t_now = self.ts()
-    delta = t_now - self.t_last
-    self.t_last = t_now
-    alpha = 1-math.exp(-delta/self.w)
+  def __call__(self, value=None, t=None):
+    assert((value is None) == (t is None))
+    if value is not None:
+      delta_t = t - self.t_last
+      self.t_last = t
+      alpha = 1-math.exp(-delta_t/self.w)
+    else:
+      alpha = None
     return self.avg(value, alpha)
 
-  def get(self):
-    return self.avg.get()
-
-
 class MovingRate:
-  def __init__(self, period, time_source=time.time):
-    self.w = period
-    self.ts = time_source
-    self.t_last = self.ts()
-    self.rate = 0
+  def __init__(self, period, t):
+    self.w = float(period)
+    self.t_last = float(t)
+    self.rate = float(0)
 
-  def __call__(self, count):
-    t_now = self.ts()
-    delta = t_now - self.t_last
-    self.t_last = t_now
-    alpha = math.exp(-delta/self.w)
-    self.rate = count + self.rate*alpha
+  def __call__(self, count=None, t=None):
+    assert((count is None) == (t is None))
+    if count is not None:
+      delta = t - self.t_last
+      self.t_last = t
+      alpha = math.exp(-delta/self.w)
+      self.rate = count + self.rate*alpha
     return self.rate
 
-  def get(self):
-    return self(0)
--- stats/test/test_counter.py
+++ stats/test/test_counter.py
@@ -17,27 +17,26 @@ class TestCounters(unittest.TestCase):
     tot = counter.CumlativeMovingAverage()
     for t,v in self.data:
       tot(v)
-    self.assertTrue(abs(tot.get()-.5)<.02)
+    self.assertTrue(abs(tot()-.5)<.02)
 
   def testExponentialMovingAverage(self):
-    avg = counter.ExponentialMovingAverage()
+    avg = counter.ExponentialMovingAverage(0.0)
     for t,v in self.data:
       avg(v, 1.0/5000.0)
-    self.assertTrue(abs(avg.get()-.5)<.02)
+    self.assertTrue(abs(avg()-.5)<.02)
 
   def testMovingAverage(self):
-    t = 0
-    aot = counter.MovingAverage(5000.0,lambda: t)
+    aot = counter.MovingAverage(5000.0, 0, 0)
     for t,v in self.data:
-      aot(v)
-    self.assertTrue(abs(aot.get()-.5)<.02)
+      aot(v,t)
+    self.assertTrue(abs(aot()-.5)<.02)
 
   def testMovingRate(self):
     t = 0
-    rat = counter.MovingRate(5000.0, lambda: t)
+    rat = counter.MovingRate(5000.0, 0)
     for t,v in self.data:
-      rat(v)
-    self.assertTrue(abs(rat.get()/5000-.5)<.02)
+      rat(v, t)
+    self.assertTrue(abs(rat()/5000-.5)<.02)
 
 if __name__ == '__main__':
   unittest.main()
