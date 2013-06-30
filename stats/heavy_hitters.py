ec09b7561611e29c60f06d39afae4821d84e7902
Andrew Thomson <andrewguy9@Andrews-MacBook-Pro.local>
2013-06-30 16:26:31 -0700

    Use sortedlist instead of python lists.

--- stats/heavy_hitters.py
+++ stats/heavy_hitters.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+from blist import sortedlist
+
 class HeavyHitters:
     def __init__(self, N):
         self.N = N
         self.c_to_wc    = {}
-        self.wc         = []
+        self.wc         = sortedlist([])
 
     def update(self, c, v):
         try:
@@ -15,13 +17,12 @@ class HeavyHitters:
             if len(self.wc) < self.N:
                 wc2 = (v,c)
             else:
-                wc = self.wc.pop()
+                wc = self.wc.pop(0)
                 self.c_to_wc.pop(wc[1])
                 wc2 = (wc[0]+v, c)
 
         self.c_to_wc[c] = wc2
-        self.wc.append(wc2)
-        self.wc.sort(reverse=True)
+        self.wc.add(wc2)
 
     def merge(self, hh):
         for (w, c) in hh.summery():
